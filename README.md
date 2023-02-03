b8e0dda8a8b8ce60d7196caf4708fc1771e971d6
unchaptered <workstation19961002@gmail.com>
2023-02-03 10:39:37 +0900

    Update(Automation) : markdown.converter.ts

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# opencv-and-computer-vison-advanced
+
+# Opencv and Computer Vision Master Class
+
+Learning about so many library for Computer Vision.
+
+- [ ] Face Detection, Face Recognition,
+- [ ] Object Detection, Object Tracking,
+- [ ] Image Classification with ANN, CNN
+- [ ] Emotion Classification with ANN
+- [ ] Image Segmentation
+- [ ] Transfer Learning and Fine Tuning 
+- [ ] Auto Encoder
+- [ ] Gesture Detection
+- [ ] Deep Dream
+- [ ] Style Trnasfer
+- [ ] GAN
+
+## Chapters 
+
+ We talked about Image & Video Processing with Deep Laerning.<br>If you want to know detailed chaptered, please check [this](./README.md#detailed-chapter) 
+
+
+### Detailed Chapter 
+
--- src/markdown.converter.ts
+++ src/markdown.converter.ts
@@ -13,7 +13,19 @@ export function convertMarkdownFile(folderMap: Map<string, string[]>): string {
     let markdown = `
 # Opencv and Computer Vision Master Class
 
-Learning about so many library for Computer Vision \n\n`;
+Learning about so many library for Computer Vision.
+
+- [ ] Face Detection, Face Recognition,
+- [ ] Object Detection, Object Tracking,
+- [ ] Image Classification with ANN, CNN
+- [ ] Emotion Classification with ANN
+- [ ] Image Segmentation
+- [ ] Transfer Learning and Fine Tuning 
+- [ ] Auto Encoder
+- [ ] Gesture Detection
+- [ ] Deep Dream
+- [ ] Style Trnasfer
+- [ ] GAN\n\n`;
 
     let chapMarkdown = '## Chapters \n\n We talked about Image & Video Processing with Deep Laerning.<br>If you want to know detailed chaptered, please check [this](./README.md#detailed-chapter) \n\n';
     let dtChapMarkdown = '### Detailed Chapter \n\n';
